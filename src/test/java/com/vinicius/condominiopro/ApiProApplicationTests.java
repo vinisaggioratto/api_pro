9348b436b1952f1fd2757a7c1632d14b638f351c
Vinicius G. Saggioratto <viniciussaggioratto@gmail.com>
2023-09-27 12:43:09 -0300

    Aplicação springboot base.

---
+++ src/test/java/com/vinicius/condominiopro/ApiProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.condominiopro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
