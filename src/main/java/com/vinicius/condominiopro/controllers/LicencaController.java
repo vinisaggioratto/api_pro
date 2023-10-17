e0ddee58a6c3fc8a4d6e23076949cfdf21ad88c2
Vinicius Gabriel Saggioratto <viniciussaggioratto@gmail.com>
2023-10-16 22:02:26 -0300

    CRUD licença ok.

--- src/main/java/com/vinicius/condominiopro/controllers/LicencaController.java
+++ src/main/java/com/vinicius/condominiopro/controllers/LicencaController.java
@@ -2,16 +2,13 @@ package com.vinicius.condominiopro.controllers;
 
 import java.util.List;
 
+import com.vinicius.condominiopro.saidaFinanceiro.SaidaFinanceiro;
+import com.vinicius.condominiopro.services.LicencaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import com.vinicius.condominiopro.licenca.DadosCadastrarLicenca;
 import com.vinicius.condominiopro.licenca.Licenca;
-import com.vinicius.condominiopro.licenca.ListarTodasLicencas;
 import com.vinicius.condominiopro.repository.LicencaRepository;
 
 import jakarta.transaction.Transactional;
@@ -19,19 +16,51 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/licencas")
+@CrossOrigin(origins = "*")
 public class LicencaController {
 
 	@Autowired
 	private LicencaRepository repository;
+
+	@Autowired
+	private LicencaService service;
 	
 	@PostMapping
 	@Transactional
-	public void cadastrar(@RequestBody @Valid DadosCadastrarLicenca dados) {
-		repository.save(new Licenca(dados));
+	public void cadastrar(@RequestBody @Valid Licenca dados) {
+		service.salvar(dados);
 	}
-	
+
 	@GetMapping
-	public List<ListarTodasLicencas> listar(){
-		return repository.findAll().stream().map(ListarTodasLicencas::new).toList();
+	public List<Licenca> listar(){
+		return repository.findAll().stream().toList();
+	}
+
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<String> atualizar(@Valid @RequestBody Licenca dados, @PathVariable Long id) {
+
+		Licenca licencaExistente = service.buscarPorId(id);
+		if (licencaExistente!=null) {
+
+			licencaExistente.setNome(dados.getNome());
+			licencaExistente.setDescricao(dados.getDescricao());
+			licencaExistente.setNumero(dados.getNumero());
+			licencaExistente.setEmissor(dados.getEmissor());
+			licencaExistente.setData_emissao(dados.getData_emissao());
+			licencaExistente.setData_validade(dados.getData_validade());
+			licencaExistente.setValido(dados.getValido());
+
+			service.salvar(licencaExistente);
+			return ResponseEntity.ok("Licença atualizada com sucesso!");
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public void excluir(@PathVariable Long id) {
+		service.deletar(id);
 	}
 }
--- src/main/java/com/vinicius/condominiopro/licenca/Licenca.java
+++ src/main/java/com/vinicius/condominiopro/licenca/Licenca.java
@@ -8,93 +8,39 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity(name = "Licenca")
 @Table(name = "licencas")
-@NoArgsConstructor
-@AllArgsConstructor
+//@NoArgsConstructor
+//@AllArgsConstructor
+@Getter
+@Setter
 @EqualsAndHashCode(of = "licenca_id")
 public class Licenca {
 
-	public Licenca(DadosCadastrarLicenca dados) {
-		super();
-		this.licenca_nome = dados.licenca_nome();
-		this.licenca_descricao = dados.licenca_descricao();
-		this.numero = dados.numero();
-		this.emissor = dados.emissor();
-		this.data_emissao = dados.data_emissao();
-		this.data_validade = dados.data_validade();
-		this.valido = dados.valido();
-	}
-	
-	@Id
+    public Licenca() {
+    }
+
+    public Licenca(Long licenca_id) {
+        this.licenca_id = licenca_id;
+    }
+
+    @Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
-    private long licenca_id;
+    private Long licenca_id;
 	
 	@Column(name = "nome")
-    private String licenca_nome;
+    private String nome;
 	
 	@Column(name = "descricao")
-    private String licenca_descricao;
+    private String descricao;
 	
     private String numero;
     private String emissor;
     private Date data_emissao;
     private Date data_validade;
     private String valido;
-    
-  //GETTER E SETTER - LOMBOK NÃO ESTÁ FUNCIONANDO
-	public long getLicenca_id() {
-		return licenca_id;
-	}
-	public void setLicenca_id(long licenca_id) {
-		this.licenca_id = licenca_id;
-	}
-	public String getLicenca_nome() {
-		return licenca_nome;
-	}
-	public void setLicenca_nome(String licenca_nome) {
-		this.licenca_nome = licenca_nome;
-	}
-	public String getLicenca_descricao() {
-		return licenca_descricao;
-	}
-	public void setLicenca_descricao(String licenca_descricao) {
-		this.licenca_descricao = licenca_descricao;
-	}
-	public String getNumero() {
-		return numero;
-	}
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
-	public String getEmissor() {
-		return emissor;
-	}
-	public void setEmissor(String emissor) {
-		this.emissor = emissor;
-	}
-	public Date getData_emissao() {
-		return data_emissao;
-	}
-	public void setData_emissao(Date data_emissao) {
-		this.data_emissao = data_emissao;
-	}
-	public Date getData_validade() {
-		return data_validade;
-	}
-	public void setData_validade(Date data_validade) {
-		this.data_validade = data_validade;
-	}
-	public String getValido() {
-		return valido;
-	}
-	public void setValido(String valido) {
-		this.valido = valido;
-	}
-    
+
 }
